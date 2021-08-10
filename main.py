317f55b3ddafae42e0c48b6d4262b4d63816160c
danyarmarkin <danila.iarmarkin@gmail.com>
2021-08-10 16:44:00 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import requests
+import json
+import time
+from math import *
+try:
+    import RPi.GPIO as gpio
+except:
+    pass
+
+lamps = [2, 3, 4, 17, 27, 22, 10, 9, 11, 14, 15, 18, 23, 24, 25, 8]
+
+value = 0
+calibrate = 0
+lampsOff = 0
+
+for i in lamps:
+    gpio.setup(i, gpio.OUT)
+
+def lamp(v, c, off):
+    global lamps
+    d = v - c
+    if d < 0:
+        d += 360
+    l = floor(d / (360 / len(lamps)))
+    if l >= len(lamps):
+        l = len(lamps) - 1
+    for i in lamps:
+        gpio.output(i, gpio.HIGH)
+    print(l)
+    ind = 0
+    if off % 2 == 0:
+        ind = 1
+    for i in range(l - floor(off / 2 - ind), l + floor(off / 2), 1):
+        j = i
+        if j < 0:
+            j += len(lamps)
+        if j >= len(lamps):
+            j -= len(lamps)
+        gpio.output(lamps[j], gpio.LOW)
+
+while True:
+    r = requests.get('https://camera-scan-e5684-default-rtdb.europe-west1.firebasedatabase.app/compassData.json?print=pretty')
+    responce = json.loads(r.text)
+    print(responce["value"])
+    value = int(responce["value"])
+    calibrate = int(responce["calibration"])
+    lampsOff = int(responce["lampsOff"])
+    lamp(value, calibrate, lampsOff)
+    time.sleep(0.005)
